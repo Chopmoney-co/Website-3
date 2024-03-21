6227ef019bd7d411c1317a02a6e8d4b5bb51048e
ntorilarry <lntori99@gmail.com>
2024-03-21 08:16:57 +0000

    added animations

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,16 +6,16 @@ const Home = () => {
   return (
     <div className="relative overflow-hidden bg-[#1F1F1F]">
       <div className="relative">
-        <div className="absolute top-[250px] right-[150px] hidden lg:block">
+        <div className="animate-bounce absolute top-[250px] right-[150px] hidden lg:block">
           <img src={Bg} alt="" />
         </div>
-        <div className="absolute top-[250px] left-[150px] hidden lg:block">
+        <div className="animate-bounce absolute top-[250px] left-[150px] hidden lg:block">
           <img src={Bg3} alt="" />
         </div>
-        <div className="absolute top-[650px] right-[450px] hidden lg:block">
+        <div className="animate-bounce absolute top-[650px] right-[450px] hidden lg:block">
           <img src={Bg1} alt="" />
         </div>
-        <div className="absolute top-[650px] left-[450px] hidden lg:block">
+        <div className="animate-bounce absolute top-[650px] left-[450px] hidden lg:block">
           <img src={Bg2} alt="" />
         </div>
       </div>
